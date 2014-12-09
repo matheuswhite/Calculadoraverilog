e593f34a6b4677c0d4effac1d21c100b6eecbbcc
Matheus Tenorio <matheustenorio@ic.com.br>
2014-12-09 20:23:47 -0300

    Display consertado!
    "Deus é bom o tempo todo"

--- HolderResult.v
+++ HolderResult.v
@@ -4,7 +4,7 @@ module HolderResult(clock, A, Sel, Out);
 	input [2:0] Sel;
 	output [7:0] Out;
 	
-	reg [7:0] Out;
+	reg [7:0] Out, reg1, reg2 = 0;
 	
 	wire wSel, wSel2, SelNot3, SelNot2, SelNot1;
 	
@@ -16,8 +16,13 @@ module HolderResult(clock, A, Sel, Out);
 	and a2(wSel2, Sel[2], SelNot2, SelNot1);
 	
 	always @(posedge clock) begin
-		if(wSel)
-			Out <= A;
+		reg1 = reg2; 
+		reg2 = A;
+		
+		if(wSel) begin
+			if(reg1 == reg2)
+				Out <= A;
+		end
 		else if(wSel2)
 			Out <= 0;
 	end
